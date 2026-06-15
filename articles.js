128e8bbbf2fd08af8d6dbe59dc6ef4c058be0b6e
liekec <lieke.corbeek@ru.nl>
2026-06-15 16:18:41 +0200

    Create articles.js

---
+++ articles.js
@@ -0,0 +1,26 @@
+const articles = [
+  {
+    id: "open-education",
+    title: "MEET-UP SIG OPEN EDUCATION: CONNECTING THE OPENS",
+    date: "2026-06-10",
+    image: "open education.png",
+    excerpt: "Open Science and Open Education are both growing, but are they also growing towards one another?",
+    link: "article.html"
+  },
+  {
+    id: "barcamp",
+    title: "OSC-NL Barcamp + National Open Science Festival 2026 🌍",
+    date: "2026-05-26",
+    image: "2.png",
+    excerpt: "This October, Delft will become the meeting place for the Dutch open science community...",
+    link: "article.html"
+  },
+  {
+    id: "turning-the-tide",
+    title: "Turning the Tide: Stories of Open Science 🌊",
+    date: "2026-04-16",
+    image: "turning the tide- stories of open science.jpeg",
+    excerpt: "Researchers share personal turning points that led them toward open science.",
+    link: "article.html"
+  }
+];
